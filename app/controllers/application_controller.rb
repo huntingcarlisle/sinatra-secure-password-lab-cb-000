60380e47d3fd61999ae052fcdc3cd568a44b10ca
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-04-19 22:00:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    @user = User.new(:username => params[:username], :password => params[:password])
-    if @user.save
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
       redirect '/login'
     else
       redirect '/failure'
